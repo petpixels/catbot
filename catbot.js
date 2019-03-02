b75eaf57a7706c7aaf986d3eeec919d15b023ad3
Alan Underwood <adunderwood@gmail.com>
2019-03-02 05:29:48 +0000

    Meow on command

--- catbot.js
+++ catbot.js
@@ -2,12 +2,50 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 
 const chan_general = "421090801393598466"
+const chan_catbot = "551271365508857866"
 
 client.on('ready', () => {
     console.log("Connected as " + client.user.tag)
 
-    var generalChannel = client.channels.get(chan_general) // Replace with known channel ID
-    generalChannel.send("Meow")
+    // List servers the bot is connected to
+    /*
+    console.log("Servers:")
+    client.guilds.forEach((guild) => {
+        console.log(" - " + guild.name)
+
+        // List all channels
+        guild.channels.forEach((channel) => {
+            console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`)
+        })
+    })
+    */
+
+    var generalChannel = client.channels.get(chan_general)
+    var catbotChannel = client.channels.get(chan_catbot)
+    //catbotChannel.send("Meow")
+})
+
+client.on('message', (receivedMessage) => {
+    // Prevent bot from responding to its own messages
+    if (receivedMessage.author == client.user) {
+        return
+    }
+
+console.log(receivedMessage.channel.id)
+    // restrict to catbot channel
+
+    if (receivedMessage.channel.id == chan_catbot) {
+
+
+    }
+
+    // Check if the bot's user was tagged in the message
+    if (receivedMessage.content.includes(client.user.toString())) {
+      // Send acknowledgement message
+      receivedMessage.channel.send("Meow")
+    }
+
+
 })
 
 // Get your bot's secret token from:
