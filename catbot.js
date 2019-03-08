4ec7b0ee02601cb7996f425635b9e51ef1e575f4
root <Alan Underwood>
2019-03-08 08:52:29 +0000

    New codebase

--- catbot.js
+++ catbot.js
@@ -1,12 +1,12 @@
-// When it says secret, it really kind of means secret...
-// so let's put it right at the top of this file here
-const catbot_secret = require("./catbot-secret")
-const bot_secret_token = catbot_secret.bot_secret_token
+const bot_secret = require('./lib/bot-secret')
+var catbot = require('./lib/bot');
 
-const Discord = require('discord.js')
-const client = new Discord.Client()
+var cat = new catbot()
 
-const catbot = require("./catbot-functions")
+const discord = require('discord.js')
+const client = new discord.Client()
+
+//const catbot = require("./lib/catbot-functions")
 const catbotUserID = "CatBot#8780"
 
 // channels (probably shouldn't be hardcoded)
@@ -15,48 +15,10 @@ const chan_general = "421090801393598466"
 const chan_catbot = "551271365508857866"
 const chan_cleverbot = "548078165936046080"
 
-const { createLogger, format, transports } = require('winston');
-require('winston-daily-rotate-file');
-
 const fs = require('fs');
 const path = require('path');
 const request = require('request');
 
-const env = process.env.NODE_ENV || 'development';
-const logDir = 'logs';
-
-// Create the log directory if it does not exist
-if (!fs.existsSync(logDir)) { fs.mkdirSync(logDir); }
-
-// const filename = path.join(logDir, 'catbot.log');
-const dailyRotateFileTransport = new transports.DailyRotateFile({
-  filename: `${logDir}/catbot-%DATE%.log`,
-  datePattern: 'YYYY-MM-DD'
-});
-
-const logger = createLogger({
-  // change level if in dev environment versus production
-  level: env === 'development' ? 'debug' : 'info',
-  format: format.combine(
-    format.timestamp({
-      format: 'YYYY-MM-DD HH:mm:ss'
-    }),
-    format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
-  ),
-  transports: [
-    new transports.Console({
-      level: 'info',
-      format: format.combine(
-        format.colorize(),
-        format.printf(
-          info => `${info.timestamp} ${info.level}: ${info.message}`
-        )
-      )
-    }),
-    dailyRotateFileTransport
-    //new transports.File({ filename })
-  ]
-});
 /*
 process.on('uncaughtException', function(err) {
   logger.debug(err)
@@ -66,46 +28,59 @@ process.on('uncaughtException', function(err) {
 });
 */
 client.on('ready', () => {
-  var generalChannel = client.channels.get(chan_general)
-  var catbotChannel = client.channels.get(chan_catbot)
+	var sayHello = true
+	var catChannel
 
-  logger.info("Connected as " + client.user.tag)
+	cat.name("Cat")
+	cat.default_reply("Meow")
+	cat.keywords("cat,kitten")
+	cat.rating("G")
+	cat.odds(.5)
+	cat.log("Connected as " + client.user.tag)
 
-  // set discord client "now playing"
-  var startPlaying = catbot.play();
-  client.user.setActivity(startPlaying)
-  logger.info("Now playing " + startPlaying)
+	// locate catbot channel
+	client.guilds.forEach((guild) => {
+		guild.channels.forEach((channel) => {
+			//console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`)
+			if (channel.name.includes("catbot")) {
+				catChannel = channel
+				// cat.channel(channel) // this doesn't work and I don't know why
+			}
+		})
+	})
 
-  // say hello on boot
-  var sayHello = catbot.reply()
-  var saidHello = false;
-  if (sayHello) {
-    // bot stays quiet in dev mode because
-    // it gets rebooted alot
-    if (env != "development") {
-      catbotChannel.send(sayHello)
-      saidHello = true;
-    }
+	// set discord client "now playing"
+	client.user.setActivity(cat.play())
 
-  }
-  logger.info("#catbot: " + sayHello)
-  logger.info("#catbot: " + saidHello + " | " + sayHello)
+	// say hello
+	if (sayHello) { cat.say("Meow", catChannel) }
+
 })
 
 // Rat on people who delete messages and save a log of it
 client.on('messageDelete', (receivedMessage) => {
-  receivedMessage.channel.send("Mao")
-  logger.info("<" + receivedMessage.channel.id + "> @catbot: Mao")
-  logger.info("Deleted message: \"" + receivedMessage + "\"")
+  cat.say("Mao")
+  cat.log("Deleted message: \"" + receivedMessage + "\"")
 })
 
 // Welcome new members
 client.on('guildMemberAdd', msg => {
-  var newUserGreeting = catbot.reply();
-  msg.guild.channels.get(chan_general).send(newUserGreeting)
+	// locate catbot channel
+	var genChannel
+	client.guilds.forEach((guild) => {
+		guild.channels.forEach((channel) => {
+			//console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`)
+			if (channel.name.includes("general")) {
+				genChannel = channel
+				// cat.channel(channel) // this doesn't work and I don't know why
+			}
+		})
+	})
 
-  logger.info("New User: " + msg)
-  logger.info("<" + receivedMessage.channel.id + "> @catbot: " + newUserGreeting)
+	cat.reply(msg.guild.channels.get(genChannel));
+
+  cat.log("New User: " + msg)
+  cat.log("<" + receivedMessage.channel.id + "> @catbot: " + newUserGreeting)
 
 })
 
@@ -117,91 +92,66 @@ client.on('message', (receivedMessage) => {
   // Prevent bot from responding to its own messages
   if (receivedMessage.author == client.user) { return } // catch and release
 
-  // Check if the bot's user was tagged in the message
-  // Always reply to messages from any channel
-  if (receivedMessage.isMentioned(client.user)) {
-    var silent = false
 
-    // Get acknowledgement message from catbot
-    var direct_input = receivedMessage.content.toLowerCase()
-    var direct_output = catbot.reply(direct_input)
+	var msg = receivedMessage.content;
+	emu.log(receivedMessage.channel + msg)
 
-    // Log acknowledgement message
-    logger.info("Tagged message received from " + receivedMessage.author.toString() + ": " + receivedMessage.content)
+	if (receivedMessage.content.includes("cat")) {
+		receivedMessage.react("🐤") // random cat emoji
+	}
 
-    var msg = receivedMessage.content;
+	// Check if the bot's user was tagged in the message
+	// Always reply to messages from any channel
+	if (receivedMessage.isMentioned(client.user)) {
+		// Get acknowledgement message from catbot
+		var direct_input = receivedMessage.content.toLowerCase()
+		var direct_output = "Meow."
 
-    // Set catbot status
-    // Play (anything)
-    if (msg.toLowerCase().includes("!pineapple")) {
-      receivedMessage.channel.send("🍍")
-      logger.info("<" + receivedMessage.channel.id + "> @catbot: 🍍")
-      silent = true // because we already sent a pineapple
-    }
+		// Log acknowledgement message
+		var msg = receivedMessage.content.toLowerCase();
 
-    if (msg.toLowerCase().includes("!gif")) {
-      var url = "https://api.giphy.com/v1/gifs/random?api_key=9nbVwPCSeiP6Hh17oPJkMCRnYpA99FUO&tag=cat%20kitten&rating=PG";
-      silent = true
+		// Really need to modularize this function... (Done!)
+		if (msg.includes("!gif")) {
+			silent = true
+			cat.gif(receivedMessage.channel, msg);
+		}
 
-      request.get({
-        url: url,
-        json: true,
-        headers: {'User-Agent': 'request'}
-      }, (err, res, data) => {
-        if (err) {
-          logger.info("Giphy request failed")
-        } else if (res.statusCode !== 200) {
-          logger.info("Giphy request succeeded")
-        } else {
-          // data is already parsed as JSON:
-          var received = data
-          // loop through each data object because there can be more than one
-          for (var data in received) {
-            var giphy = received[data];
-            receivedMessage.channel.send(giphy.embed_url)
-            logger.info("<" + receivedMessage.channel.id + "> @catbot: " + giphy.embed_url)
+		if (msg.includes("!sticker")) {
+			silent = true
+			cat.sticker(receivedMessage.channel, msg);
+		}
 
-            // console.log(data+": "+received[data]);
-          }
+		if (msg.includes("!play")) {
+			silent = true
+			cat.play(msg)
+		}
 
-        }
-      });
-    }
+		if (cb_input.includes("!treat")) {
+			silent = true
+			receivedMessage.channel.send(catbot.randomTreatEmoji())
+		}
 
-    if (msg.toLowerCase().includes("!play")) {
-      var aPlay = msg.split(" ") // because it's not lowercase
-      var tmpPlay = ""
-      var playLoc = 0
+		// Pineapple
+		if ((cb_input.includes("!pineapple")) || (cb_input == "🍍")) {
+			silent = true
+			receivedMessage.channel.send("🍍")
+		}
 
-      // double iteration has got to be a bad idea
-      // but if it's stupid and it works it's not stupid
-      for (var i = 0; i < aPlay.length; i++) {
-        if (aPlay[i] == "!play") {
-          playLoc = i+1
-        }
-      }
+		// Fish
+		if (cb_input.includes("!fish")) {
+			silent = true
+			receivedMessage.channel.send(catbot.randomFishEmoji())
+		}
 
-      for (var i = playLoc; i < aPlay.length; i++) {
-        tmpPlay = tmpPlay + " " + aPlay[i]
-      }
 
-      logger.info("Now playing " + tmpPlay)
+		if (!(silent)) {
+			emu.reply(receivedMessage.channel, receivedMessage.content)
+		}
+	} else {
 
-      // set discord client "now playing"
-      client.user.setActivity(tmpPlay)
-      silent = true
-    }
 
-    // Send acknowledgement message
-    if (!(silent)) {
-      receivedMessage.channel.send(direct_output)
-      logger.info("<" + receivedMessage.channel.id + "> @catbot: " + cb_msg)
-    } else {
-      logger.info("<" + receivedMessage.channel.id + "> @catbot (silenced): " + cb_msg)
-    }
+	}
 
-    suppressOutput = true
-  }
 
   // In the catbot channel
   if ((receivedMessage.channel.id == chan_catbot) || (receivedMessage.channel.id == chan_cleverbot)) {
@@ -218,7 +168,7 @@ client.on('message', (receivedMessage) => {
       var outputFlag = false
 
       // log input message
-      logger.info("Input message: " + receivedMessage.content)
+      cat.log("#catbot: " + receivedMessage.content)
 
       // Catbot meows to all mentions of cat in the catbot channel
       if (cb_input.includes("cat"))       { cb_output.push(cb_msg); outputFlag = true }
@@ -243,8 +193,9 @@ client.on('message', (receivedMessage) => {
       if (cb_input.includes("fuck"))     { cb_output.push("Quack!"); outputFlag = true }
 
       // Output Message (if any)
-
-      logger.info("Potential output messages: " + cb_output)
+      if (cb_output) {
+        cat.log("Potential output messages: " + cb_output)
+      }
 
       // if ouput flag is set, check for output
       if (outputFlag) {
@@ -296,15 +247,15 @@ client.on('message', (receivedMessage) => {
             }
           }
 
-          logger.info("<" + receivedMessage.channel.id + "> @catbot: " + retString)
+          cat.log("<" + receivedMessage.channel.id + "> @catbot: " + retString)
 
           if (retString && (!(suppressOutput))) {
-            receivedMessage.channel.send(retString)
+            cat.say(receivedMessage.channel, retString)
           }
 
           // log suppressed message
           if (retString && (suppressOutput)) {
-            logger.info("<" + receivedMessage.channel.id + "> @catbot: (Message Suppressed) " + retString)
+            cat.log("<" + receivedMessage.channel.id + "> @catbot: (Message Suppressed) " + retString)
           }
         }
       }
@@ -312,29 +263,22 @@ client.on('message', (receivedMessage) => {
   } else {
     // React to "cat" in messages outside of the catbot channel
     // This should probably be a different bot.
-    var catEmoji = catbot.react(receivedMessage.content)
+    /*
+		var catEmoji = catbot.react(receivedMessage.content)
     if (catEmoji) {
       for (var i = 0; i < catEmoji.length; i++) {
         logger.info("Reacted to: <" + receivedMessage.channel.id + "> " + receivedMessage.content + " with emoji " + catEmoji[i])
         receivedMessage.react(catEmoji[i])
       }
     }
+		*/
 
-    // Cat always replies to direct messages outside of catbot channel
+    // Cat always replies to direct messages in the catbot channel
     // I wonder how long that's going to last...
     if (receivedMessage.content.includes("551263363884122122")) { // why?
-      logger.info('@tagged')
-      var cb_msg = catbot.reply()
-      receivedMessage.channel.send(cb_msg)
-    }
-
-    // occasionally meow
-    if (receivedMessage.content.toLowerCase().includes("cat")) {
-      var randomReply = Math.random();
-      if (randomReply < .1) {
-        var cb_msg = catbot.reply(receivedMessage.content)
-        receivedMessage.channel.send(cb_msg)
-      }
+      cat.log('@tagged')
+      cat.reply(receivedMessage.channel, receivedMessage.content)
+      //receivedMessage.channel.send(cb_msg)
     }
   }
 
@@ -342,20 +286,18 @@ client.on('message', (receivedMessage) => {
   if (receivedMessage.content.includes(client.user.toString())) {
     // Send acknowledgement message
     outputFlag = true
-    cb_output.push(catbot.reply(receivedMessage.content.toLowerCase()))
+    cat.reply(receivedMessage.channel, receivedMessage.content)
   }
 
   // Random global meom
-
   var randomGlobalReply = Math.random();
-  if (randomReply < .05) {
-    var cb_msg = catbot.reply(receivedMessage.content)
-    logger.info("Random Global Reply: " + cb_msg + " to " + receivedMessage.content)
+  if (randomGlobalReply < .05) {
+		cat.reply(receivedMessage.channel, receivedMessage.content)
+    cat.log("Random Global Reply: " + cb_msg + " to " + receivedMessage.content)
     receivedMessage.channel.send(cb_msg)
   }
-
 
   // console.log(receivedMessage.channel.id)
 })
 
-client.login(bot_secret_token)
+client.login(bot_secret.bot_secret_token)
